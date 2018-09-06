5db96de8a52be02861ceb06639a28585a44a5b8e
Chia-Pin Tseng <chiapintseng@gmail.com>
2018-09-06 14:55:09 +0800

    Init test matter.js as physics lib

---
+++ js/TestDemo.js
@@ -0,0 +1,26 @@
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies;
+
+var engine = Engine.create();
+
+var render = Render.create({
+                element: document.body,
+                engine: engine,
+                options: {
+                    width: 800,
+                    height: 400,
+                    wireframes: false
+                }
+             });
+
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var ballA = Bodies.circle(380, 100, 40, 10);
+var ballB = Bodies.circle(460, 10, 40, 10);
+var ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
+
+World.add(engine.world, [boxA, ballA, ballB, ground]);
+
+Engine.run(engine);
+Render.run(render);
