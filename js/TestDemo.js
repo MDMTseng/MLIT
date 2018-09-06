91bf6769fdabdf4f519156eed649af6ed2627e8d
Chia-Pin Tseng <chiapintseng@gmail.com>
2018-09-06 19:10:32 +0800

    Add basic player beam casting mechanism as playground basis

--- js/TestDemo.js
+++ js/TestDemo.js
@@ -13,12 +13,13 @@
                   engine: engine,
                   options: {
                       width: 800,
-                      height: 400,
+                      height: 800,
                       wireframes: false
                   }
                });
-  engine.world.gravity.y = 0;
-  var boxA = Bodies.rectangle(400, 200, 80, 40);
+  console.log(render);
+  engine.world.gravity.y = 0.1;
+  var boxA = Bodies.rectangle(400, 200, 40, 40);
 
   boxA.restitution=0.5;
   Matter.Body.setAngle(boxA, 30 / 180 * Math.PI)
@@ -33,12 +34,15 @@
 
   ballA.restitution=0.5;
   ballB.restitution=0.5;
-  var ground = Bodies.rectangle(400, 380, 810, 60, { isStatic: true });
-
-  World.add(engine.world, [boxA, ballA, ballB, ground]);
+  let showWallPix=20;
+  var ground = Bodies.rectangle(render.canvas.width/2, render.canvas.height, render.canvas.width, showWallPix, { isStatic: true });
+  var ceiling = Bodies.rectangle(render.canvas.width/2,           0, render.canvas.width, showWallPix, { isStatic: true });
+  var lwall = Bodies.rectangle(0, render.canvas.height/2, showWallPix, render.canvas.height, { isStatic: true });
+  var rwall = Bodies.rectangle(render.canvas.width, render.canvas.height/2, showWallPix, render.canvas.height, { isStatic: true });
+  var AllPlayers = [boxA, ballA, ballB];
+  World.add(engine.world, [boxA, ballA, ballB, ground,ceiling,lwall,rwall]);
   Engine.run(engine);
   Render.run(render);
-  console.log(Render);
 
   ballA.onCollide((pair)=>
   {
@@ -49,7 +53,6 @@
     var normRay = Matter.Vector.normalise(r);
     var ray = normRay;
     var point = Matter.Vector.add(ray, start);
-    var advL = dist/2;
 
     for(var i = 0; i < dist; i+=stepSize){
       ray = Matter.Vector.mult(normRay, i);
@@ -65,65 +68,64 @@
         return {point: ray, body: bod};
       }
     }
-    return;
-  }
-  function raycast_naive_X(bodies, start, r, dist, stepSize=10){
-    let cast_ret = raycast_naive(bodies, start, r, dist, stepSize);
-
-    var normRay = Matter.Vector.normalise(r);
-    if(typeof cast_ret == 'undefined')
-    {
-      return {point: {x:start.x+normRay.x*dist,y:start.y+normRay.y*dist}};
-    }
-    return cast_ret;
+    return {point: {x:start.x+normRay.x*dist,y:start.y+normRay.y*dist}};
   }
 
+
+  console.log(ballB);
   Matter.Events.on(render, "afterRender", (evt)=>{
-    //console.log(evt);
-
+    //console.log(mainOBj);
+    var allPlayers = AllPlayers;
 
     let ctx = evt.source.canvas.getContext("2d");
-    let time = evt.timestamp;
-    var bs = Matter.Composite.allBodies(engine.world);
 
-    var fromVec = {x:200, y:200};
-    let range = 500;
-    var beams = 40;
-    let angle_int=30*Math.PI/beams/180;
-    let init_angle=time/500-angle_int*beams/2;
-    for(var i=0; i < beams; i++){
-      let angle = init_angle+angle_int*i;
-      var toVec = {x:Math.sin(angle), y:Math.cos(angle)};
-      var  ret = raycast_naive_X(bs,fromVec,toVec,range,10);
+    //let time = evt.timestamp;
+    let range = 200;
+    var beams = 10;
+    let angle_int=180*Math.PI/beams/180;
 
-      ctx.beginPath();
+    allPlayers.forEach(function(player) {
 
-      if(typeof ret.body == 'undefined')
-      {
-          ctx.strokeStyle="green";
+      var bs = Matter.Composite.allBodies(engine.world);
+      const index = bs.findIndex(body => body.id === player.id);
+      bs.splice(index, 1);
+
+      var fromVec = {x:player.position.x, y:player.position.y};
+      let init_angle=-player.angle-angle_int*(beams-1)/2;
+
+      let Addforce = {x:0,y:0};
+      for(var i=0; i < beams; i++){
+        let angle = init_angle+angle_int*i;
+        var toVec = {x:Math.sin(angle), y:Math.cos(angle)};
+        var  ret = raycast_naive(bs,fromVec,toVec,range,10);
+
+        ctx.beginPath();
+
+        if(typeof ret.body == 'undefined')
+        {
+            ctx.strokeStyle="green";
+        }
+        else
+        {
+            ctx.strokeStyle="red";
+        }
+
+        ctx.moveTo(fromVec.x,fromVec.y);
+        ctx.lineTo(ret.point.x,ret.point.y);
+        ctx.stroke();
+
+        let dist = Math.hypot(ret.point.x-fromVec.x,ret.point.y-fromVec.y);
+        if(dist<range*0.8)
+        {
+          let forceMag = -(1-dist)*0.000003*player.mass;
+          let force = {x:fromVec.x-ret.point.x,y:fromVec.y-ret.point.y};
+          force = Matter.Vector.normalise(force);
+          force = Matter.Vector.mult(force, forceMag);
+          Addforce = Matter.Vector.add(Addforce, force);
+        }
       }
-      else
-      {
-          ctx.strokeStyle="red";
-      }
-
-      ctx.moveTo(fromVec.x,fromVec.y);
-      ctx.lineTo(ret.point.x,ret.point.y);
-      ctx.stroke();
-    }
-
-    /*
-    var cast = Matter.Query.ray(bs, fromVec, toVec);
-    console.log(cast);
-
-    if(cast.length>0)
-    {
-      ctx.moveTo(fromVec.x,fromVec.y);
-      ctx.lineTo(cast[0].body.position.x,cast[0].body.position.y);
-      ctx.stroke();
-    }
-    ctx.stroke();*/
-
+      Matter.Body.applyForce(player, player.position, Addforce);
+    });
   });
 
 
