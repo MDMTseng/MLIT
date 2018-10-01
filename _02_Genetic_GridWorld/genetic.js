f853936420e522c77c7a1dca9ffbcefa029b65ba
Chia-Pin Tseng <chiapintseng@gmail.com>
2018-10-01 15:39:51 +0800

    Grid world init

---
+++ _02_Genetic_GridWorld/genetic.js
@@ -0,0 +1,148 @@
+
+function drawPlot(env,creature)
+{
+    setPlot(WorldPlot,1,env);
+    setPlot(WorldPlot,0,creature);
+    setPlotYLimit(WorldPlot,0,{min:-2,max:5});
+    setPlotYLimit(WorldPlot,1,{min:-2,max:5});
+    WorldPlot.update();
+}
+
+
+
+
+let ExpRange={min:-2*2*Math.PI,max:2*2*Math.PI}
+
+function getEnvFeedBack(param)
+{
+    //let x = 0.5*(-param*param/30+3)*Math.sin(param);
+    //let x = Math.sin(param*param/10)+param/20;
+    let x =Math.sin(param);
+    return x;
+    //return Math.sin(param*param/10)+param/20;
+}
+
+function GenerateEnvArr(ExpRange,samples=100)
+{
+    let arr = [];
+
+
+    for(let i=0;i<samples;i++)
+    {
+        let alpha = 1.0*i/(samples-1);
+        let x = (1-alpha)*ExpRange.min+(alpha)*ExpRange.max;
+        arr.push({x:x,y:getEnvFeedBack(x)});
+    }
+    return arr;
+}
+
+let worldEnv = GenerateEnvArr(ExpRange);
+
+
+
+function generate_creatures(ExpRange,count)
+{
+    let creatures=[];
+    for(let i=0;i<count;i++)
+    {
+        let x=Math.random()*(ExpRange.max-ExpRange.min)+ExpRange.min;
+        creatures.push({x:x,y:0});
+    }
+    return creatures
+}
+
+function SpownNewPopulation(expRange=ExpRange,count=150)
+{
+    return generate_creatures(expRange,count);
+}
+let creatures=SpownNewPopulation({min:0,max:1});
+
+
+
+
+drawPlot(worldEnv,creatures);
+
+
+function EVOLVE()
+{
+
+    creatures=creatures.map(
+        (body)=>{
+            if(body.x>ExpRange.min && body.x<ExpRange.max)
+                body.y=getEnvFeedBack(body.x);
+            else
+            {
+                body.y=-1000;
+                body.x=ExpRange.min;
+            }
+            return body;
+        }
+    );
+    drawPlot(worldEnv,creatures);
+
+
+    creatures.sort(function(a, b){return b.y-a.y});
+    let topN=Math.floor(creatures.length*0.5);
+    let leastAcceptedScore = creatures[topN].y;
+    let topN_List=creatures.slice(0, topN);
+
+    let topScore = topN_List[0].y;
+
+    console.log("top:"+topScore, "mid:"+leastAcceptedScore);
+    
+    creatures=creatures.map(
+        (body)=>{
+            if(body.y<leastAcceptedScore)
+            {
+                let parent1=topN_List[Math.floor(Math.random()*topN)].x;
+                let parent2=topN_List[Math.floor(Math.random()*topN)].x;
+                body.x = 
+                    (parent1)+
+                    (Math.random()-0.5);
+
+                if(Math.random()<0.1)
+                    body.x+=20*(Math.random()-0.5);
+            }
+            body.y=0;
+            return body;
+        }
+    );
+}
+
+
+function ResetCreature()
+{
+    creatures=SpownNewPopulation({min:-6,max:-4});
+
+    drawPlot(worldEnv,creatures);
+}
+function NextStep()
+{
+    EVOLVE();
+}
+
+var AutoHdl=null;
+function ToggleAuto()
+{
+    if(AutoHdl == null)
+        AutoHdl= setInterval(EVOLVE, 100);
+    else
+    {
+        clearInterval(AutoHdl);
+        AutoHdl=null;
+    }
+}
+
+/*
+let creatures=[];
+for(let i=0;i<20;i++)
+{
+    creatures.push(Math.random()*2*Math.PI);
+}
+setCreaturePlot(myScatter,creatures,()=>0);
+
+myScatter.update();
+
+
+
+*/
---
+++ _02_Genetic_GridWorld/gridWorld.js
@@ -0,0 +1,125 @@
+
+
+
+function GridWorld(theWorld)
+{
+    this.theWorld=theWorld;
+    
+    
+
+    this.reward = function( x, y){
+        if(x<0 || y<0)return -10000;
+        if(x>=this.theWorld[0].length || 
+           y>=this.theWorld.length)return -10000;
+        return  this.theWorld[y][x];
+    };
+
+
+
+    this.decision = function( x, y, policy, exploration_factor=0.1 ){
+        return  [
+            policy[y][x][0]+this.rand(exploration_factor),
+            policy[y][x][1]+this.rand(exploration_factor)];
+    };
+
+
+    this.rand=function(scale=1)
+    {
+        return (Math.random()-0.5)*2*scale;
+    };
+
+    this.RandomPolicy_Init = function(scale){
+        let policy=[];
+        for(let i=0;i<this.theWorld.length;i++)
+        {
+            let sub_policy=[];
+            for(let j=0;j<this.theWorld[i].length;j++)
+            {
+                sub_policy.push([this.rand(scale),this.rand(scale)]);
+            }
+            policy.push(sub_policy);
+        }
+        return  policy;
+    };
+
+
+    this.Policy_Mix = function(outPolicy ,policies){
+
+        for(let i=0;i<this.theWorld.length;i++)
+        {
+            for(let j=0;j<this.theWorld[i].length;j++)
+            {
+                let policySum=[0,0];
+                let WCount=0;
+
+                for(let k=0;k<policies.length;k++)
+                {
+                    let W = Math.random();
+                    policySum[0] += W*policies[k][i][j][0];
+                    policySum[1] += W*policies[k][i][j][1];
+                    WCount +=W;
+                }
+                outPolicy[i][j][0]=policySum[0]/WCount;
+                outPolicy[i][j][1]=policySum[1]/WCount;
+            }
+        }
+        return  outPolicy;
+    };
+
+    this.Policy_Mutate = function(outPolicy,inPolicy,scale=0.1,xratio=0.01,xscale=1){
+        
+        for(let i=0;i<this.theWorld.length;i++)
+        {
+            for(let j=0;j<this.theWorld[i].length;j++)
+            {
+                outPolicy[i][j][0]=inPolicy[i][j][0]+this.rand(scale);
+                outPolicy[i][j][1]=inPolicy[i][j][1]+this.rand(scale);
+                if(Math.random()<xratio)
+                {
+                    outPolicy[i][j][0]+=this.rand(xscale);
+                    outPolicy[i][j][1]+=this.rand(xscale);
+                }
+            }
+        }
+        return  outPolicy;
+    };
+}
+
+let test=false;
+
+if(test){
+    let gw = new GridWorld([
+        [0,0,0,-1],
+        [0,0,0,-1],
+        [0,0,0,-1],
+        [0,0,100,-1]
+    ]);
+
+
+
+    let policy = gw.RandomPolicy_Init();
+    console.log(JSON.stringify(policy[2]));
+    policy = gw.Policy_Mutate(policy,policy);
+    console.log(JSON.stringify(policy[2]));
+
+    for(let life=0;life<100;life++)
+    {
+        let score=0;
+        let location={x:0,y:0};
+        for(let i=0;i<100;i++)
+        {
+            let decesion = gw.decision( location.x, location.y, policy);
+            location.y+=(decesion[0]<0)?-1:1;
+            location.x+=(decesion[1]<0)?-1:1;
+        
+            let reward = gw.reward(location.x, location.y);
+            score+=reward+1;
+            if(reward<-0)break;
+
+        }
+
+        console.log(score);
+
+        policy = gw.Policy_Mutate(policy,policy);
+    }
+}
