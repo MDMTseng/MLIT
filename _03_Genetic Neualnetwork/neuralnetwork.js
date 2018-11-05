185c685a17be042ccd28f37b3a5eb97279acf6f9
Chia-Pin Tseng <chiapintseng@gmail.com>
2018-11-05 20:34:59 +0800

    Add neural network

--- _03_Genetic Neualnetwork/neuralnetwork.js
+++ _03_Genetic Neualnetwork/neuralnetwork.js
@@ -1,24 +1,96 @@
 
 
-function random(scale)
+function random(scale=1)
 {
     return scale*2*(Math.random()-0.5);
 }
 
-function CreateNeuralNetLayer(NodeCount)
+function CreateNeuralNode(InputCount)
+{
+    //Create single node
+    //Example:
+    //InputCount =3
+    //[0.23423, 0.56,  0.7,  0.3 ]
+
+    let arr = [];
+    for(let i=0;i<InputCount+1;i++)
+    {
+        arr.push(random(1));
+    }
+    return {w:arr}
+}
+function CreateNeuralNetLayer(previousNodeCount,NodeCount)
 {
     //Create single layer
+
+    //NodeCount =3
+    //[{Node} {Node} {Node}]
+
+    let arr = [];
+    let node_value = [];
+    for(let i=0;i<NodeCount;i++)
+    {
+        arr.push(CreateNeuralNode(previousNodeCount));
+        node_value.push(0);
+    }
+    return {nodes:arr,node_value:node_value}
 }
-function CreateNeuralNet()
+function CreateNeuralNet(network_shape)
 {
     //Create complete network
+    //network_shape = [1,4,4,4,1] to create a network that has one input, 
+    //one output and has three hidden layers with 4 nodes within it.
 
+    let layers=[];
+
+    for(let i=1;i<network_shape.length;i++)
+    {
+        layers.push(CreateNeuralNetLayer(network_shape[i-1],network_shape[i]));
+    }
+
+    return {layers:layers};
 }
 
 
 
-function NeuralNetForwardPass()
+function NeuralNetNodeForwardPass(preLayer,currentNode)
+{
+    let valueSum=0;
+    let i=0;
+    for(i=0;i<preLayer.node_value.length;i++)
+    {
+        valueSum+=preLayer.node_value[i]*currentNode.w[i];
+    }
+    valueSum+=currentNode.w[currentNode.w.length-1];
+    if(valueSum<0)valueSum=0;//ReLu
+    return valueSum;
+}
+
+
+function NeuralNetLayerForwardPass(preLayer,currentLayer)
+{
+
+    for(i=0;i<currentLayer.nodes.length;i++)
+    {
+        currentLayer.node_value[i]=
+            NeuralNetNodeForwardPass(preLayer,currentLayer.nodes[i]);
+    }
+    return currentLayer.node_value;
+}
+
+function NeuralNetForwardPass(input,network)
 {
     //Create complete network
-
+    let inputLayer = {node_value:input};
+    NeuralNetLayerForwardPass(inputLayer,network.layers[0]);
+    for(let i=1;i<network.layers.length;i++)
+    {
+        NeuralNetLayerForwardPass(network.layers[i-1],network.layers[i]);
+    }
+    return network.layers[network.layers.length-1].node_value;
 }
+
+
+let network = CreateNeuralNet([3,5,6,7,1]);
+console.log(network);
+console.log(NeuralNetForwardPass([4,0.7,8.8798],network));
